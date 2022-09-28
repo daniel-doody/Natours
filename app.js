e94d2d6cc4b287b6fc0b3b118bbc3531939fd16d
Daniel Doody <you@example.com>
2022-09-28 13:09:55 -0400

    creating the API get/post requests for tours data, validated by postman

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json("Hello, from the server side!");
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post("/", (req, res) => {
-  res.send("Posting to this endpoint...");
+// app.get("/", (req, res) => {
+//   res.status(200).json("Hello, from the server side!");
+// });
+
+// app.post("/", (req, res) => {
+//   res.send("Posting to this endpoint...");
+// });
+
+app.get("/api/tours", (req, res) => {
+  res.json({
+    status: "success",
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 app.app.listen((port = 3000), () => {
